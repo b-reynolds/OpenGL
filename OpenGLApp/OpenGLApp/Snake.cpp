addbf10aa80c9ba255165e4faf899d6a9001f637
Ben Reynolds <benr420@gmail.com>
2016-12-23 20:15:40 +0000

    Add Food to Snake class & minor refactor

--- OpenGLApp/OpenGLApp/Snake.cpp
+++ OpenGLApp/OpenGLApp/Snake.cpp
@@ -1,73 +1,119 @@
 #include "Snake.h"
-
+#include <random>
+#include "Random.h"
 
 void Snake::setDirection(const Direction& direction)
 {
-	switch (direction)
+	if(direction == UP && this->direction != DOWN)
 	{
-	case UP:
 		velocity = Vector3D(0.0f, 0.0f, -2.0f);
-		break;
-	case RIGHT:
-		velocity = Vector3D(2.0f, 0.0f, 0.0f);
-		break;
-	case DOWN:
-		velocity = Vector3D(0.0f, 0.0f, 2.0f);
-		break;
-	case LEFT:
-		velocity = Vector3D(-2.0f, 0.0f, 0.0f);
-		break;
+		this->direction = direction;
 	}
-	this->direction = direction;
+	else if(direction == RIGHT && this->direction != LEFT)
+	{
+		velocity = Vector3D(2.0f, 0.0f, 0.0f);
+		this->direction = direction;
+	}
+	else if(direction == DOWN && this->direction != UP)
+	{
+		velocity = Vector3D(0.0f, 0.0f, 2.0f);
+		this->direction = direction;
+	}
+	else if(direction == LEFT && this->direction != RIGHT)
+	{
+		velocity = Vector3D(-2.0f, 0.0f, 0.0f);
+		this->direction = direction;
+	}
 }
 
 void Snake::update()
 {
+	elapsed = glutGet(GLUT_ELAPSED_TIME);
+	if(elapsed - lastMove < SPEED)
+	{
+		return;
+	}
+
 	snake.move(velocity);
+	lastMove = elapsed;
 
+	Vector3D currentPosition = snake.getPosition();
 
-	bool t = (lastPosition == snake.getPosition());
-	if(t == false)
+	if(currentPosition.x < MIN_X || currentPosition.x > MAX_X || currentPosition.z < MIN_Z || currentPosition.z > MAX_Z)
 	{
-		printf("B");
+		reset();
+		return;
 	}
 
-	if (lastPosition != snake.getPosition())
+	for(auto & position : tailPositions)
 	{
-		previousPositions.push_back(snake.getPosition());
-	}
-	
-	if(previousPositions.size() > tailLength)
-	{
-		previousPositions.erase(previousPositions.begin());
+		if(currentPosition == position)
+		{
+			reset();
+			return;
+		}
 	}
 
+	if(currentPosition == food.getPosition())
+	{
+		tailLength++;
+		int foodX = 0;
+		do { foodX = Random::getRandomInt(MIN_X, MAX_X); } while (abs(foodX) % 2 != 0);
+		int foodZ = 0;
+		do { foodZ = Random::getRandomInt(MIN_Z, MAX_Z); } while (abs(foodZ) % 2 != 0);
+		food.setPosition(Vector3D(foodX, POS_Y, foodZ));
+	}
 
-	lastPosition = snake.getPosition();
+	tailPositions.push_back(currentPosition);
+	if(tailPositions.size() > tailLength)
+	{
+		tailPositions.erase(tailPositions.begin());
+	}
 }
 
 void Snake::draw()
 {
 	Vector3D currentPosition = snake.getPosition();
 
-	for(int i = 0; i < previousPositions.size(); ++i)
+	for(int i = 0; i < tailPositions.size(); ++i)
 	{
-		snake.setPosition(previousPositions[i]);
+		snake.setPosition(tailPositions[i]);
 		snake.draw();
 	}
 
 	snake.setPosition(currentPosition);
 	snake.draw();
 
+	food.draw();
+
 }
 
 Snake::Snake()
 {
-	tailLength = 5;
-	texture = Texture2D("Textures/grass.bmp");
-	snake = Cube(Vector3D(0.0f, -13.0f, 0.0f), Vector3D(SIZE, SIZE, SIZE), texture);
+	txtrSnake = Texture2D("Textures/snake.bmp");
+	snake = Cube(Vector3D(0.0f, -13.0f, 0.0f), Vector3D(SIZE, SIZE, SIZE), txtrSnake);
+
+	txtrFood = Texture2D("Textures/food.bmp");
+	food = Cube(Vector3D(0.0f, -13.0f, 0.0f), Vector3D(SIZE, SIZE, SIZE), txtrFood);
+
+	reset();
 }
 
-Snake::~Snake()
+void Snake::reset()
 {
+
+	int startX = 0;
+	do { startX = Random::getRandomInt(MIN_X, MAX_X); } while (abs(startX) % 2 != 0);
+	int startZ = 0;
+	do { startZ = Random::getRandomInt(MIN_Z, MAX_Z); } while (abs(startZ) % 2 != 0);
+	snake.setPosition(Vector3D(startX, POS_Y, startZ));
+
+	int foodX = 0;
+	do { foodX = Random::getRandomInt(MIN_X, MAX_X); } while (abs(foodX) % 2 != 0);
+	int foodZ = 0;
+	do { foodZ = Random::getRandomInt(MIN_Z, MAX_Z); } while (abs(foodZ) % 2 != 0);
+	food.setPosition(Vector3D(foodX, POS_Y, foodZ));
+
+	tailLength = 2;
+	tailPositions.clear();
 }
--- OpenGLApp/OpenGLApp/Snake.h
+++ OpenGLApp/OpenGLApp/Snake.h
@@ -3,7 +3,7 @@
 #include <vector>
 
 class Snake
-{
+{ 
 
 public:
 
@@ -14,21 +14,35 @@ public:
 	void draw();
 
 	Snake();
-	~Snake();
 
 private:
 
 	const GLfloat SIZE = 1.0f;
+	const int SPEED = 50;
+
+	const float MIN_X = -30;
+	const float MAX_X = 0;
+	const float MIN_Z = -30;
+	const float MAX_Z = 0;
+	const float POS_Y = -13;
+
+	int lastMove;
+	int speed;
+	int elapsed;
 
 	Vector3D velocity;
-	Vector3D lastPosition;
 	Direction direction;
 	int tailLength;
 
-	std::vector<Vector3D> previousPositions;
+	std::vector<Vector3D> tailPositions;
 
-	Texture2D texture;
+	Texture2D txtrSnake;
+	Texture2D txtrFood;
+
 	Cube snake;
+	Cube food;
+
+	void reset();
 
 };
 
