60cf59aa14a752dd21d506187a7029e37c855a32
Ben Reynolds <benr420@gmail.com>
2016-12-23 17:26:04 +0000

    Remove generic initializations from Texture2D class

--- OpenGLApp/OpenGLApp/Texture2D.cpp
+++ OpenGLApp/OpenGLApp/Texture2D.cpp
@@ -24,18 +24,9 @@ bool Texture2D::isLoaded() const
 
 void Texture2D::initialize(char* filePath)
 {
-	// Define wrapping behaviour
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	// Define filtering behaviour
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
-
+	// Load the texture
 	texture = ilutGLLoadImage(filePath);
 
-	// Define how texture is sent down the rendering pipeline
-	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
 
 	initialized = true;
 }
